7be3fc2a591fec0685e3fcbcac88c28a907ffa0e
huzhou <huzhou.v@gmail.com>
2022-10-24 20:11:22 +0800

    add error api.

--- errs/error.go
+++ errs/error.go
@@ -1,15 +1,43 @@
 package errs
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
+// Throw method will panic the given err if it's not nil.
 func Throw(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+// Throwf method will panic the given err if it's not nil, and using custom text as panic message.
 func Throwf(err error, format string, v ...any) {
 	if err != nil {
 		panic(fmt.Sprintf(format, v...))
 	}
+}
+
+// New method will create an error using the given string and arguments.
+func New(s string, v ...any) error {
+	if v == nil {
+		return errors.New(s)
+	}
+	return errors.New(fmt.Sprintf(s, v))
+}
+
+// Must method expect there must no error, i.e. err argument is nil, if it is not, then
+// panic it, otherwise return the first argument.
+func Must[T any](v T, err error) T {
+	Throw(err)
+	return v
+}
+
+// Get will return the given error when it is not nil, otherwise return nil.
+func Get(err error) error {
+	if err != nil {
+		return err
+	}
+	return nil
 }

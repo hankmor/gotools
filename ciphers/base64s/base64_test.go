6f7cb31f418656f88e3062dae46dee48a5303667
huzhou <huzhou.v@gmail.com>
2023-03-10 13:17:00 +0800

    add base64urluint

---
+++ ciphers/base64s/base64_test.go
@@ -0,0 +1,129 @@
+package base64s_test
+
+import (
+	"fmt"
+	"github.com/huzhouv/gotools/ciphers/base64s"
+	"reflect"
+	"testing"
+)
+
+var s = "abcdefghijjklmnopqrstuvwxyz0123456789`~-_=+[]\\{}|;':\",./<>?"
+var sw = "YWJjZGVmZ2hpamprbG1ub3BxcnN0dXZ3eHl6MDEyMzQ1Njc4OWB+LV89K1tdXHt9fDsnOiIsLi88Pj8="
+var rw = "YWJjZGVmZ2hpamprbG1ub3BxcnN0dXZ3eHl6MDEyMzQ1Njc4OWB+LV89K1tdXHt9fDsnOiIsLi88Pj8"
+var s1 = "hello, 这是中文!"
+var sw1 = "aGVsbG8sIOi/meaYr+S4reaWhyE="
+var rw1 = "aGVsbG8sIOi/meaYr+S4reaWhyE"
+
+func TestBase(t *testing.T) {
+	// YWJjZGVmZ2hpamprbG1ub3BxcnN0dXZ3eHl6MDEyMzQ1Njc4OWB+LV89K1tdXHt9fDsnOiIsLi88Pj8=
+	fmt.Println(base64s.StdEncoding.Encode([]byte(s)))
+	// YWJjZGVmZ2hpamprbG1ub3BxcnN0dXZ3eHl6MDEyMzQ1Njc4OWB+LV89K1tdXHt9fDsnOiIsLi88Pj8
+	fmt.Println(base64s.RawStdEncoding.Encode([]byte(s)))
+
+	// aGVsbG8sIOi/meaYr+S4reaWhyE=
+	fmt.Println(base64s.StdEncoding.Encode([]byte(s1)))
+	// aGVsbG8sIOi/meaYr+S4reaWhyE
+	fmt.Println(base64s.RawStdEncoding.Encode([]byte(s1)))
+}
+
+func Test_base64raw_Decode(t *testing.T) {
+	type args struct {
+		str    string
+		strict []bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{name: "case1", args: args{str: rw, strict: []bool{true}}, want: []byte(s), wantErr: false},
+		{name: "case2", args: args{str: rw1, strict: []bool{true}}, want: []byte(s1), wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ba := base64s.RawStdEncoding
+			got, err := ba.Decode(tt.args.str, tt.args.strict...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Decode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Decode() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_base64raw_Encode(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "case1", args: args{b: []byte(s)}, want: rw},
+		{name: "case2", args: args{b: []byte(s1)}, want: rw1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ba := base64s.RawStdEncoding
+			if got := ba.Encode(tt.args.b); got != tt.want {
+				t.Errorf("Encode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_base64std_Decode(t *testing.T) {
+	type args struct {
+		str    string
+		strict []bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{name: "case1", args: args{str: sw, strict: []bool{true}}, want: []byte(s), wantErr: false},
+		{name: "case2", args: args{str: sw1, strict: []bool{true}}, want: []byte(s1), wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ba := base64s.StdEncoding
+			got, err := ba.Decode(tt.args.str, tt.args.strict...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Decode() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Decode() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_base64std_Encode(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "case1", args: args{b: []byte(s)}, want: sw},
+		{name: "case2", args: args{b: []byte(s1)}, want: sw1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ba := base64s.StdEncoding
+			if got := ba.Encode(tt.args.b); got != tt.want {
+				t.Errorf("Encode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ ciphers/base64s/base64url_test.go
@@ -0,0 +1,30 @@
+package base64s_test
+
+import (
+	"fmt"
+	"github.com/huzhouv/gotools/ciphers/base64s"
+	"github.com/huzhouv/gotools/testool"
+	"reflect"
+	"testing"
+)
+
+const (
+	plaintext = "abcdefghijjklmnopqrstuvwxyz0123456789`~-_=+[]\\{}|;':\",./<>?"
+	base64enc = "YWJjZGVmZ2hpamprbG1ub3BxcnN0dXZ3eHl6MDEyMzQ1Njc4OWB-LV89K1tdXHt9fDsnOiIsLi88Pj8"
+)
+
+func Test_Base64UrlEncode(t *testing.T) {
+	lg := testool.Wrap(t)
+	lg.Case("testing base64s.URLEncoding.Encode")
+	enc := base64s.RawURLEncoding.Encode([]byte(plaintext))
+	fmt.Println(enc)
+	lg.Require(base64enc == enc, "result should match")
+}
+
+func Test_Base64UrlDecode(t *testing.T) {
+	lg := testool.Wrap(t)
+	lg.Case("testing base64s.URLEncoding.Decode")
+	dec, err := base64s.RawURLEncoding.Decode(base64enc, true)
+	lg.Require(err == nil, "requires no error")
+	lg.Require(reflect.DeepEqual([]byte(plaintext), dec), "results should be matched")
+}
